e62721086ef11a98f513a55ab85110970a28a55f
Jason Singh <jsingh214@msn.com>
2014-07-25 13:07:16 -0400

    Test scripts and alternates

--- cleanup/cleanupAlternate.ps1
+++ cleanup/cleanupAlternate.ps1
@@ -1,14 +1,4 @@
-﻿#==============================================================================
-# File:    cleanupHomeDirectoriestudents.ps1
-# Author:  Jason Singh
-# Date:    7/24/2014
-# Purpose: Cleans up student home directories from a specified school.
-#==============================================================================
-
-#Imports AD module...
-Import-Module ActiveDirectory
-
-#Takes in a user inputted variable to determine which school students to clean up home directories for...
+﻿#Takes in a user inputted variable to determine which school students to clean up home directories for...
 $school = Read-Host 'Which school needs student home directories cleaned up?(Please answer, PPS, UMS, MBS, CMS, or CHS.)'
 
 #Uses and if else statement to determine which school after input...
@@ -42,11 +32,20 @@ exit
 $users = Get-ADuser -filter * -SearchBase $searchOU -properties HomeDirectory
 
 #Using a foreach loop it will look in each students home directory and delete everything except the home directory folder...
-foreach ($user in $users) { 
-$userHomeFolder = -join $user.homedirectory
-If (Test-Path $userHomeFolder) { 
-Get-ChildItem $user.HomeDirectory | Remove-Item -Recurse
+#Comment from email: Script should only delete the folders where the folder name and the samaccountname match, so if you have any users without a homedir set nothing will get deleted.
+#Remeber to remove the -whatif only after ensuring the results are as you desire.
+
+
+foreach ($user in $users) 
+{ 
+$sam = (Get-Aduser -identity $user).samaccountname
+$homeDir = (Get-Aduser -Identity $user -Properties HomeDirectory).homedirectory
+$dir = Split-Path $homeDir -Leaf
+If($sam -eq $dir)
+{
+Get-ChildItem $user.homedirectory | Remove-Item -Recurse -Force -WhatIf
 }
 }
+
 
 Write-Host 'Student file cleanup successful.'

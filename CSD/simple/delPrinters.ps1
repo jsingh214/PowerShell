f7f5ecdc804daf78727648563c96a8afd3e320b7
Jason Singh <jsingh214@msn.com>
2016-04-11 08:31:38 -0400

    After rep cleanup

--- CSD/simple/delPrinters.ps1
+++ CSD/simple/delPrinters.ps1
@@ -3,13 +3,7 @@ $printers = Get-WmiObject -Class Win32_Printer
 
 # Delete Printers
 $printers | ForEach-Object {
-    if ($_.Name -Match "\\chs1"){
-        $_.Delete()
-    }
-}
-
-$printers | ForEach-Object {
-    if ($_.Name -Match "\\cms1"){
+    if ($_.Name -Match "\\(server_name)"){
         $_.Delete()
     }
 }
--- simple/delPrintersPPS.ps1
+++
@@ -1,9 +0,0 @@
-﻿# Get Printers
-$printers = Get-WmiObject -Class Win32_Printer
-
-# Delete Printers
-$printers | ForEach-Object {
-    if ($_.Name -Match "\\pps2"){
-        $_.Delete()
-    }
-}

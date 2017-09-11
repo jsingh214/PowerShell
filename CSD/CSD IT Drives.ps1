0aa480cd9b73c7cb3a4e8364ad4d4b3ead0b2ff4
Jason Singh <jsingh214@msn.com>
2017-09-11 08:49:03 -0400

    Clean Up 2017

---
+++ CSD/CSD IT Drives.ps1
@@ -0,0 +1,23 @@
+﻿#==============================================================================
+# File:    CSDITDrives.ps1
+# Author:  Jason Singh
+# Date:    7/25/2016
+# Purpose: Map drives for CSD IT Admins as a logon script.
+#==============================================================================
+
+$user = $env:username
+
+#Imports AD module...
+Import-Module ActiveDirectory
+
+#Maps S Drive to user
+if (Get-ADUser -Filter {SamAccountName -like $user} -SearchBase "OU=Users,OU=CSD IT,DC=csd,DC=local"){
+    New-PSDrive –Name “S” –PSProvider FileSystem –Root “\\CSD160\CSDshared” –Persist
+
+}
+
+#Maps U Drive to user
+if (Get-ADUser -Filter {SamAccountName -like $user} -SearchBase "OU=Users,OU=CSD IT,DC=csd,DC=local"){
+    New-PSDrive –Name “U” –PSProvider FileSystem –Root “\\CSD160\ITAdmins” –Persist
+
+}
